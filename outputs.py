eb0634e1c3096bee0c1c7578b8c16dc5a6b82a55
Sam Zimmerman <samzimmerman@me.com>
2023-05-01 12:03:54 -0700

    Remove productSize Improve error reporting and logging

--- outputs.py
+++ outputs.py
@@ -3,13 +3,28 @@ import json
 from tqdm import tqdm
 from pathlib import Path
 import logging
+import logging.handlers as handlers
+import os
 
-logging.basicConfig(filename='output.log', encoding='utf-8', level=logging.INFO)
+logfile_name = "output.log"
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+rollCheck = os.path.isfile(logfile_name)
+handler = handlers.RotatingFileHandler(logfile_name, backupCount=5, encoding="utf-8")
+formatter = logging.Formatter("%(levelname)s - %(message)s")
+
+handler.setFormatter(formatter)
+handler.setLevel(logging.INFO)
+logger.addHandler(handler)
+
+if rollCheck:
+    logger.handlers[0].doRollover()
 
 contents_files = Path("data/contents/").glob("*.yaml")
 new_files = Path("data/products").glob("*.yaml")
 
 products_contents = {}
+valid_contents = ["sealed", "pack", "deck", "other", "variable", "card_count", "card"]
 
 for file in contents_files:
 	with open(file, "rb") as f:
@@ -18,11 +33,14 @@ for file in contents_files:
 		if contents:
 			if data["code"] not in products_contents:
 				products_contents[data["code"]] = {}
-			if "size" not in contents:
-			    logging.info("%s/%s missing product size", data["code"], product)
-			products_contents[data["code"]][product] = contents
+			if ("pack" in contents or "deck" in contents) and "card_count" not in contents:
+			    logger.info("%s/%s missing card count", data["code"], product)
+			if any([c not in valid_contents for c in contents.keys()]):
+			    logger.error("%s/%s has invalid content codes", data["code"], product)
+			else:
+			    products_contents[data["code"]][product] = contents
 		else:
-			logging.info("%s/%s missing contents", data["code"], product)
+			logger.info("%s/%s missing contents", data["code"], product)
 
 with open("outputs/contents.json", "w") as outfile:
 	json.dump(products_contents, outfile)
--- product_compiler.py
+++ product_compiler.py
@@ -3,24 +3,32 @@ from tqdm import tqdm
 import yaml
 from pathlib import Path
 import logging
+import logging.handlers as handlers
+import os
+
+logfile_name = "product.log"
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+rollCheck = os.path.isfile(logfile_name)
+handler = handlers.RotatingFileHandler(logfile_name, backupCount=5, encoding="utf-8")
+formatter = logging.Formatter("%(levelname)s - %(message)s")
+
+handler.setFormatter(formatter)
+handler.setLevel(logging.INFO)
+logger.addHandler(handler)
+
+if rollCheck:
+    logger.handlers[0].doRollover()
 
 alt_codes = {
 	"CON_": "con"
 }
-logging.basicConfig(filename='product.log', encoding='utf-8', level=logging.INFO)
 
 parentPath = Path("mtgJson/AllSetfiles/")
 files = parentPath.glob("*.json")
 t = tqdm(files)
 codes = set()
 for file in t:
-	'''
-	with open(file, 'rb') as ifile:
-		booster_types = ijson.kvitems(ifile, "data.booster")
-		types = list(dict(booster_types).keys())
-		for t in types:
-			print(file, "booster:", t)
-	'''
 	output_file = Path("data/contents/").joinpath(file.with_suffix(".yaml").name)
 	if output_file.is_file():
 		with open(output_file, 'r') as f:
@@ -37,11 +45,11 @@ for file in t:
 		existing_names = [k for k in products.keys()]
 		for p in sealed_product:
 			if p["name"] not in products:
-				logging.info("Added new product %s/%s", file.stem, p["name"])
+				logger.info("Added new product %s/%s", file.stem, p["name"])
 				products[p["name"]] = []
 		for n in existing_names:
 		    if n not in mtgjson_names:
-		        logging.info("Product %s/%s no longer present in MTGJson data", file.stem, n)
+		        logger.info("Product %s/%s no longer present in MTGJson data", file.stem, n)
 	code = alt_codes.get(file.stem.lower(), file.stem.lower())
 	with open(output_file, 'w') as write:
 		yaml.dump({"code": code, "products": products}, write)
